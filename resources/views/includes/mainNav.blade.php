528e7de98fcda920183c134cf0cca68381ea79be
jeg24@njit.edu <Edu1983jegl*>
2018-11-06 22:21:40 -0500

    Feature: add mainNav

---
+++ resources/views/includes/mainNav.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">WebSiteName
+            </a>
+        </div>
+        <li class="nav navbar-nav">
+        <li class="active"><a href="{{route ('home')}}">Home</a></li>
+        <li
+        <a href="{{route ('about')}}">About</a></li>
+        <li
+        <a href="{{route ('contact')}}">Contact</a></li>
+        <li
+        <a href="/register">Register</a></li>
+        <li
+        <a href="/login">Login</a></li>
+        </ul>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.home');
+})->name('home');
+Route::get('/about', function () {
+    return view('pages.about');
+})->name('about');
+Route::get('/contact', function () {
+    return view('pages.contact');
+})->name('contact');
+
